e18d6e5099bba5baa501cfd28335d203627ca8b3
mjarraya <mjarraya@student.42.fr>
2020-02-24 12:55:57 +0100

    Post model and routes

--- models/Post.js
+++ models/Post.js
@@ -1,1 +1,34 @@
 /* We'll write the schema and register our model for the posts here */
+const mongoose = require("mongoose");
+
+// const { Schema } = mongoose;
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    content: String,
+    type: {
+      enum: ["link", "text"],
+      type: String
+    },
+    // _author: {
+    //   type: Schema.Types.ObjectId,
+    //   ref: "User"
+    // },
+    upvote_count: Number
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at"
+    }
+  }
+);
+
+const Post = mongoose.model("Post", postSchema);
+
+module.exports = Post;
--- routes/posts.js
+++ routes/posts.js
@@ -1,5 +1,82 @@
 const router = require("express").Router();
-
+const Post = require("../models/Post");
 /* Here we'll write the routes for the posts */
+
+router.get("/posts", (req, res) => {
+  Post.find()
+    .then(posts => {
+      res.json(posts);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.get("/posts/:id", (req, res) => {
+  const postId = req.params.id;
+
+  Post.findById(postId)
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.post("/posts", (req, res) => {
+  // const title = req.body.title;
+  // const type = req.body.type;
+  // const content = req.body.content
+  const { title, type, content } = req.body;
+
+  Post.create({
+    title: title,
+    type: type,
+    content: content,
+    upvote_count: 0
+    // author: req.user._id
+  })
+    .then(postDocument => {
+      res.json(postDocument);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.post("/posts/:id/upvote", (req, res) => {
+  const postId = req.params.id;
+
+  Post.findByIdAndUpdate(postId, { $inc: { upvote_count: 1 } }, { new: true })
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
+
+router.delete("/posts/:id/upvote", (req, res) => {
+  const postId = req.params.id;
+
+  Post.findByIdAndUpdate(postId, { $inc: { upvote_count: -1 } }, { new: true })
+    .then(post => {
+      res.json(post);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: err.message
+      });
+    });
+});
 
 module.exports = router;
