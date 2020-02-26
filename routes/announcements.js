277682a41732a697f2f0ce95ddf01af0654574bc
Martin Schmal <martinschmal@gmail.com>
2020-02-26 17:06:32 +0100

    route update

--- routes/announcements.js
+++ routes/announcements.js
@@ -1,4 +1,5 @@
 const router = require("express").Router();
+
 const Announcement = require("../models/Announcement");
 
 // READ all and sort
--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,12 @@
 const router = require("express").Router();
 const usersRoutes = require("./users");
 const postsRoutes = require("./posts");
+const annnouncementsRoutes = require("./announcements");
 
 router.use("/api/auth", usersRoutes);
 router.use("/api", postsRoutes);
+
+router.use("/announcements/delete", annnouncementsRoutes);
+router.use("/announcements", annnouncementsRoutes);
 
 module.exports = router;
