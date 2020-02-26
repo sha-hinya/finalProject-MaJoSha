a9dc50fb4c6427a0a7ef239b50a99eaaf73d803e
jonathansaudhof <jonathan@jonathansaudhof.de>
2020-02-26 18:00:58 +0100

    merged and combined everything propperly

--- bin/seeds.js
+++ bin/seeds.js
@@ -1,45 +1,85 @@
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
-// const Post = require("../models/Post");
-// const posts = require("../bin/posts.json");
+const User = require('../models/User');
+const Announcement = require('../models/Announcement');
+const announcements = require('../bin/announcements.json');
 
-const Announcement = require("../models/Announcement");
-const announcements = require("../bin/announcements.json");
+// Stack of promisses
+const promises = [];
 
-// const Document = require("../models/Document");
-// const documents = require("../bin/documents.json");
-
-mongoose.connect("mongodb://localhost:27017/MaJoSha", () => {
+mongoose.connect(process.env.MONGODB_URI, () => {
   //mongoose.connect(process.env.MONGODB_URI, () => {
-  console.log("Connected to DB");
+  console.log('Connected to DB');
 });
 
-// Post.collection.drop();
+const bcryptSalt = 10;
+const salt = bcrypt.genSaltSync(bcryptSalt);
+const hashPass = bcrypt.hashSync('1234', salt);
+
+const newUsers = [
+  // normal client : Max Musterman
+  {
+    lastName: 'Mustermann',
+    firstName: 'Max',
+    email: 'max.mustermann@example.com',
+    password: hashPass,
+    phone: '4917112345678',
+    accessRole: 'client',
+    property: [],
+    _upvotes: [],
+  },
+
+  // admin : Armin Admin
+
+  {
+    lastName: 'Admin',
+    firstName: 'Armin',
+    email: 'Armin.Admin@example.com',
+    password: hashPass,
+    phone: '4917212345678',
+    accessRole: 'admin',
+    property: [],
+    _upvotes: [],
+  },
+
+  // moderator : Melanie Moderator
+
+  {
+    lastName: 'Moderator',
+    firstName: 'Melanie',
+    email: 'Melanie.Moderator@example.com',
+    password: hashPass,
+    phone: '4917312345678',
+    accessRole: 'moderator',
+    property: [],
+    _upvotes: [],
+  },
+];
+
+User.collection.drop();
+
+promises.push(
+  User.create(newUsers).then((result) => {
+    console.log(result);
+  }),
+);
+
+console.log('JSON:', announcements);
 Announcement.collection.drop();
-// Document.collection.drop();
+promises.push(
+  Announcement.create(announcements)
+    .then((result) => {
+      console.log(`Created ${result.length} announcements`);
+      mongoose.connection.close();
+    })
+    .catch((err) => {
+      console.log(err);
+    }),
+);
 
-// Post.create(posts)
-//   .then(result => {
-//     console.log(`Created ${result.length} posts`);
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
-
-Announcement.create(announcements)
-  .then(result => {
-    console.log(`Created ${result.length} announcements`);
-    mongoose.connection.close();
-  })
-  .catch(err => {
-    console.log(err);
-  });
-
-// Document.create(documents)
-//   .then(result => {
-//     console.log(`Created ${result.length} documents`);
-//     mongoose.connection.close();
-//   })
-//   .catch(err => {
-//     console.log(err);
-//   });
+Promise.all(promises).then((result) => {
+  console.log('Seeds finished');
+  mongoose.connection.close();
+});
--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,15 @@
 /* We'll centralize our routes imports to this file to keep our code clean */
 
-
 const router = require('express').Router();
 const authRoutes = require('./auth');
 const userRoutes = require('./users');
 const postsRoutes = require('./posts');
-const annnouncementsRoutes = require("./announcements");
-
+const annnouncementsRoutes = require('./announcements');
 
 router.use('/api/auth', authRoutes);
 router.use('/api/user', userRoutes);
 router.use('/api', postsRoutes);
-
-router.use("/announcements", annnouncementsRoutes);
+// needed to add API to the path
+router.use('/api/announcements', annnouncementsRoutes);
 
 module.exports = router;
