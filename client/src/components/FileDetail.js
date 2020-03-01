2520d29ae7bdd87beca1081ab91956217f12afbd
sha-hinya <sha.haeckel@gmail.com>
2020-03-01 17:22:23 +0100

    add style to file card

--- client/src/components/FileDetail.js
+++ client/src/components/FileDetail.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Card } from "@material-ui/core";
+import SimpleCard from "./filesCard";
 
 class FileDetail extends Component {
   state = {
@@ -28,11 +30,15 @@ class FileDetail extends Component {
 
     return (
       <div>
-        <h2 style={{ border: "1px solid black" }}>{file.title}</h2>
-        <p>{file.property}</p>
-        <img src={file.url} alt={file.title} />
-        <p>created on {new Date(file.created_at).toDateString()}</p>
-        <p> created on {new Date(file.created_at).toDateString()}</p>
+        {/* <SimpleCard {...this.props} /> */}
+        <div className="fileCardsDetail">
+          <h2>{file.title}</h2>
+          <p>{file.property}</p>
+          {/* <div></div> */}
+          <p>created on {new Date(file.created_at).toDateString()}</p>
+          <p> created on {new Date(file.created_at).toDateString()}</p>
+          <img src={file.url} alt={file.title} />
+        </div>
       </div>
     );
   }
