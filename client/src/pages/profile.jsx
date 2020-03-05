8266ea781d8da2c87ecde46498873dc2d8686270
Martin Schmal <martinschmal@gmail.com>
2020-03-05 14:11:16 +0100

    fix

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -3,11 +3,7 @@ import Profile from "../components/Profile.js";
 import { Container } from "@material-ui/core";
 
 export default class profiles extends Component {
-  // componentDidMount() {
-  //   this.props.setPageTitle("Profile");
-  // }
   render() {
-    // console.log("RENDER PROFILE ", this.props);
     return (
       <Container>
         <Profile profile={this.props} setUser={this.props.setUser} />
