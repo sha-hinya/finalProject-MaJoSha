e3162f2736e55515726961e8746af37a3c93826d
JonathanSaudhof <jonathan@jonathansaudhof.de>
2020-03-05 17:02:18 +0100

    deploy v0.25

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8a323d5c6faacc66a3ca75cbed3d887d.js"
+  "/precache-manifest.0e073836c628b4b2c55cec3882933db4.js"
 );
 
 self.addEventListener('message', (event) => {
